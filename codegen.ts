fb9573d43789a2515aeace287068bc3bb90fcf26
José Manuel Piñeiro García <josempga@ext.inditex.com>
2023-09-28 08:55:51 +0100

    Add dependencies and include codege graphql scripts

---
+++ codegen.ts
@@ -0,0 +1,24 @@
+
+import type { CodegenConfig } from '@graphql-codegen/cli';
+
+const config: CodegenConfig = {
+  overwrite: true,
+  schema: "http://localhost:3000/graphql",
+  documents: "src/client/infrastructure/graphql/documents/**/*.graphql",
+  generates: {
+    "src/client/infrastructure/graphql/generated/index.ts": {
+      plugins: ['typescript', 'typescript-operations', 'typescript-graphql-request'],
+      config: {
+        rawRequest: true
+      },
+    },
+    "src/server/infrastructure/graphql/generated/index.ts": {
+      plugins: ['typescript', 'typescript-resolvers'],
+      config: {
+        useIndexSignature: true
+      },
+    }
+  }
+};
+
+export default config;
