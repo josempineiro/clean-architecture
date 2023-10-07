83f4441e1a06f0b0f605ee9328c2227db35a9216
José Manuel Piñeiro García <josempga@ext.inditex.com>
2023-10-07 12:30:19 +0100

    Fix responsive styles

--- src/core/presentation/components/forms/fields/radiogroup-field.tsx
+++ src/core/presentation/components/forms/fields/radiogroup-field.tsx
@@ -28,7 +28,7 @@ export function RadioGroupField({
       {options.map((option) => {
         const checked = Boolean(value && value === option.value)
         return(
-          <li key={option.value} className="w-full border-r pr-3 last:pr-0 last:border-none border-gray-200 dark:border-gray-600">
+          <li key={option.value} className="w-full border-b sm:border-r pr-3 last:pr-0 last:border-none border-gray-200 dark:border-gray-600">
               <div className="flex items-center">
                   <input  {...rest} id={option.value} type="radio" value={option.value} checked={checked} onChange={handleChangeOption} className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500" />
                   <label htmlFor={option.value} className="w-full py-1.5 ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">{option.label}</label>
